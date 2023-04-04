ef066862bbaad113817118dc588106dbcf68f25c
motoneo45 <102189233+motoneo45@users.noreply.github.com>
2023-04-04 10:00:23 +0200

    Update Zad2.cy.js
    Poprawka

--- Zad2.cy.js
+++ Zad2.cy.js
@@ -1,17 +1,15 @@
-//Polecenie logowania:
+/// <reference types="cypress" />
 
 Cypress.Commands.add('logowanie', (email, password) => {
  cy.get ('#user_email').type(email);
  cy.get ('#user_password').type(password);
  cy.get('.eckniwg2').click(); 
-
-//Test1
-/// <reference types="cypress" />
+})
 
 beforeEach(() => {
  cy.visit('https://www.edu.goit.global/account/login')
   })
- it('logowanko do goit', () => {
+ it('logowanko do goit podstawowe dane', () => {
  cy.pause();
  cy.logowanie('user888@gmail.com', '1234567890')
  cy.get('#open-navigation-menu-mobile').click()
@@ -19,14 +17,11 @@ cy.get('.e1phyiqy2').last().click();
 
   })  
 
-//Test2
-/// <reference types="cypress" />
 
 beforeEach(() => {
  cy.visit('https://www.edu.goit.global/account/login')
   })
- it('logowanko do goit', () => {
- cy.pause();
+ it('logowanko do goit zawansowane dane', () => {
  cy.logowanie(' testowyqa@qa.team', 'QA!automation-1');
  cy.get('#open-navigation-menu-mobile').click();
  cy.get('.e1phyiqy2').last().click();
